45376497ea45eea2ba0bc9f28c4dbe070944e461
marcuspereiradev <vinnycoast@gmail.com>
2019-07-21 12:29:06 -0300

    created list component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import List from './List';
 
 class Tasks extends Component {
   render() {
     return (
       <Row>
-        <Col xs={{ span: 8, offset: 2 }} className='tasks_list'>
-          <p className='title'>To-do</p>
-          List...
-           </Col>
-        <Col xs={{ span: 8, offset: 2 }} className='tasks_list'>
-          <p className='title'>Done</p>
-          List...
-           </Col>
+        <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
+          <p className="title">To-do</p>
+          <List tasks={[{'title': 'Criar Header Criar Header Criar Header', 'done': false}, {'title': 'Criar footer', 'done': false}, {'title': 'Criar footer', 'done': false}]}/>
+        </Col>
+        <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
+          <p className="title">Done</p>
+          <List tasks={[{'title': 'Criar Header Criar Header Criar Header', 'done': true}, {'title': 'Criar footer', 'done': true}, {'title': 'Criar footer', 'done': true}]}/>
+        </Col>
       </Row>
     );
   }
