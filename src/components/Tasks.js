4ac05f331dac6346ef56b6fe8df98fe2565053bf
marcuspereiradev <vinnycoast@gmail.com>
2019-07-21 14:18:02 -0300

    Create a new task with modal

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import List from './List';
+import CreateTask from './CreateTask';
+import Button from 'react-bootstrap/Button';
+
 
 class Tasks extends Component {
   constructor(props) {
     super(props);
     this.state = {
       tasks: []
-    }
+    };
     this.loadTasks = this.loadTasks.bind(this);
   }
 
   async loadTasks() {
-    let response = await fetch('http://localhost:3001/tasks');
+    let response = await fetch(`http://localhost:3001/tasks`);
     const tasks = await response.json();
-    this.setState({ tasks: tasks })
+    this.setState({ tasks: tasks });
   }
 
   componentDidMount() {
@@ -28,10 +31,12 @@ class Tasks extends Component {
         <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
           <p className="title">To-do</p>
           <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter((task) => task.done != true)} />
+          <CreateTask loadTasks={this.loadTasks} />
         </Col>
         <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
           <p className="title">Done</p>
           <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter((task) => task.done == true)} />
+          <Button variant="red" className="float-right remove_tasks_btn">Remove all tasks</Button>
         </Col>
       </Row>
     );
